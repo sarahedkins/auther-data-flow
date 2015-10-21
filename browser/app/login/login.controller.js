d2fc46dc753f2a6c724aa07f74528f6678a64efd
Sarah Edkins <edkins.sarah@gmail.com>
2015-10-21 14:56:58 -0400

    Up to signup and login controllers

---
+++ browser/app/login/login.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+app.controller('LoginCtrl', function ($scope) {
+    $scope.emailInput;
+    $scope.passwordInput;
+});
--- browser/utils/contenteditable/contenteditable.directive.js
+++ browser/utils/contenteditable/contenteditable.directive.js
@@ -9,9 +9,12 @@ app.directive('contenteditable', function () {
 			function read() {
 				ngModel.$setViewValue(element.html());
 			}
+
 			ngModel.$render = function () {
 				element.html(ngModel.$viewValue || '');
 			};
+
+
 			element.bind('blur keyup change', function () {
 				scope.$apply(read);
 			});
